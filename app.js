8e935768073c52c97766354db5222579991a5ccc
Jeroen Verstegen <jeroen@jota.nl>
2020-02-06 20:55:40 +0100

    wip

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const pools = require('./public/data/teams.json')
 const wedstrijden = require('./public/data/wedstrijden.json')
+const moment = require('moment')
 
+moment.locale('nl');
+moment.updateLocale('nl', {
+    weekdaysShort: ["zo", "ma", "di", "wo", "do", "vr", "za", "zo"]
+});
 app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/public'));
@@ -10,7 +15,8 @@ app.get('/', (req, res) => {
   res.render('index', {
     title: "Laurierboom Voetbalpool",
     pools: pools,
-    weds: wedstrijden
+    weds: wedstrijden,
+    moment: moment
   });
 })
 
